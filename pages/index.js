3ce20745039c3be4f6380049895268b098993e4d
Umair Malik <malikumairooo2@gmail.com>
2023-04-06 20:40:03 +0500

    maybe responsive

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,84 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className= 'bg-[#f5fbff] flex items-center justify-between w-full'>
+
+      <body className='bg-grey-500 text-black '>
+
+        <div className="container mx-auto px-5 py-8">
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+
+            <div className="grid shadow-md px-4 py-4">
+
+              <h2 class="text-lg font-bold text-gray-800">Feature 1</h2>
+              <p class="text-gray-600 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in purus magna. Nullam vel faucibus lectus. </p>
+            </div>
+
+            <div className="grid shadow-md px-4 py-4">
+              <h2 class="text-lg font-bold text-gray-800">Feature 2</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in purus magna. Nullam vel faucibus lectus.</p>
+            </div>
+
+            <div className="grid shadow-md px-4 py-4">
+              <h2 class="text-lg font-bold text-gray-800">Feature 3</h2>
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in purus magna. Nullam vel faucibus lectus.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <footer className='bg-white gap-4 shadow-md'>
+          <div className="container mx-auto px-3 py-3">
+            <p className='text-gray-500 text-center'>
+              &copy; 2023 my Website All rights reserved!
+            </p>
+          </div>
+
+        </footer>
+
+
+       
+
+      </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+
+
+  )
+}
+
+// <header class="bg-white shadow-sm py-4">
+//   <div class="container mx-auto px-4">
+//     <h1 class="text-2xl font-bold text-gray-800">My Website</h1>
+//   </div>
+// </header>
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <main className= 'bg-[#f5fbff] flex items-center justify-between w-full'>
        
        <div className="left  px-12 py-6 sm:px-16 sm:py-6  md:px-20 md:py-10 lg:px-24 lg:py-16 xl:px-28 xl:py-20">
             <h1 className='font-medium text-base w-40  sm:font-medium sm:text-2xl sm:w-60 md:font-bold md:text-3xl md:w-80 lg:font-extrabold lg:text-4xl lg:w-96 xl:font-extrabold xl:text-5xl xl:w-1/2'>
@@ -32,7 +109,7 @@ export default function Home() {
           
         </div> 
        
-          <div className="right border rounded-lg md:items-start md:mt-6  flex items-center justify-end mt-40">
+          <div className="right border rounded-lg md:items-start md:mt-6   flex items-center justify-end mt-40">
               <div className="pictureIcon w-10  mt-3">
                 <img className='rounded-full' src="/images/umair.jpeg" alt="image"/>
               </div>
@@ -47,10 +124,16 @@ export default function Home() {
 
    
       </main> 
+
+
+    <div className="second">
+      
+    </div>
+
       
     </>
   )
-}
+} */}
 
 
 
@@ -59,4 +142,3 @@ export default function Home() {
 
 
 
- 
